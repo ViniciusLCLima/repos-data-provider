01da567fa76cb47b6aac9fb2340c2ec23f3c9495
ViniciusLCLima <87040226+ViniciusLCLima@users.noreply.github.com>
2024-05-03 14:56:44 -0300

    Get pathname and proj Name

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
+import GITHUB_API_KEY from "./auth.js"
+
+const getReqPathName = (req) =>{
+  const reqState = req[Reflect.ownKeys(req).find(key => key.toString() === "Symbol(state)")];
+  return reqState.url.pathname
+}
+
+const getProjName = (pathname) =>{
+  return pathname.substring(5)
+}
 
 export function GET(req) {
     console.log("--------------------\n")
-    console.log(typeOf(req));
+    const REQ_PATH_NAME = getReqPathName(req)
+    const PROJ_NAME = getProjName(REQ_PATH_NAME)
+
     return new Response(`Hello from ${process.env.VERCEL_REGION}`);
   }
+
