60e3902a39d5eb7235a975887a076b81f6d12674
ViniciusLCLima <87040226+ViniciusLCLima@users.noreply.github.com>
2024-05-03 16:25:30 -0300

    exchanging input place to be in query

--- api/index.js
+++ api/index.js
@@ -1,19 +1,39 @@
-import GITHUB_API_KEY from "./auth.js"
+import {authCode as GITHUB_API_KEY} from "./auth.js"
+import { Octokit } from "@octokit/core"
 
-const getReqPathName = (req) =>{
-  const reqState = req[Reflect.ownKeys(req).find(key => key.toString() === "Symbol(state)")];
-  return reqState.url.pathname
+const authObj = new Octokit ({
+  auth: GITHUB_API_KEY
+})
+
+const MY_GIT_HUB_USERNAME = "ViniciusLCLima"
+const MY_GIT_HUB_URL = `https://github.com/${MY_GIT_HUB_USERNAME}/`
+
+const getProjLiveUrlAndDescr = async (projRepoName)=>{
+  const response = await authObj.request(`GET /repos/${MY_GIT_HUB_USERNAME}/${projRepoName}`,{
+  owner: MY_GIT_HUB_USERNAME,
+  name: projRepoName,
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+  console.log(typeof await response)
+return {
+      descr: response.data.description,
+      liveUrl: response.data.homepage
+  };
 }
 
-const getProjName = (pathname) =>{
-  return pathname.substring(5)
+const getRepoName = (req) =>{
+  const reqState = req[Reflect.ownKeys(req).find(key => key.toString() === "Symbol(state)")];
+  return reqState.url.searchParams.get('repoName')
 }
 
 export function GET(req) {
     console.log("--------------------\n")
-    const REQ_PATH_NAME = getReqPathName(req)
-    const PROJ_NAME = getProjName(REQ_PATH_NAME)
-
-    return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+    const PROJ_REPO_NAME = getRepoName(req)
+    console.log(PROJ_REPO_NAME)
+    let projData
+    getProjLiveUrlAndDescr(PROJ_REPO_NAME).then(x => console.log(x))
+    return new Response(JSON.stringify(projData));
   }
 
